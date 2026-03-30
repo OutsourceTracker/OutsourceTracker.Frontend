352cff0ca45cf9e3d5dbb5cd5cde6dad15b33d32
Austin Vandersluis <austin@vandersluis.club>
2026-03-30 10:43:45 -0700

    Refactor: BaseBackendService to remove MSAL-specific dependencies

--- src/Services/BaseBackendService.cs
+++ src/Services/BaseBackendService.cs
@@ -1,12 +1,8 @@
-﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
-
-namespace OutsourceTracker.Services;
+﻿namespace OutsourceTracker.Services;
 
 public abstract class BaseBackendService
 {
     private HttpClient HttpClient { get; }
-    private NavigationManager Navigation { get; }
 
     protected ILogger Logger { get; }
 
@@ -15,22 +11,13 @@ public abstract class BaseBackendService
         ILoggerFactory factory = services.GetRequiredService<ILoggerFactory>();
         Logger = factory.CreateLogger(GetType());
         HttpClient = services.GetRequiredService<IHttpClientFactory>().CreateClient("API");
-        Navigation = services.GetRequiredService<NavigationManager>();
     }
 
     protected virtual async Task<HttpResponseMessage> SendMessage(HttpRequestMessage message, CancellationToken cancellationToken)
     {
-        try
-        {
-            Logger.LogDebug("Sending {METHOD} to {URI}", message.Method, message.RequestUri);
-            var response = await HttpClient.SendAsync(message, cancellationToken);
-            Logger.LogDebug("Received Response: {CODE}", response.StatusCode);
-            return response;
-        }
-        catch (AccessTokenNotAvailableException ex)
-        {
-            ex.Redirect();
-            return new HttpResponseMessage(System.Net.HttpStatusCode.Unauthorized);
-        }
+        Logger.LogDebug("Sending {METHOD} to {URI}", message.Method, message.RequestUri);
+        var response = await HttpClient.SendAsync(message, cancellationToken);
+        Logger.LogDebug("Received Response: {CODE}", response.StatusCode);
+        return response;
     }
 }
