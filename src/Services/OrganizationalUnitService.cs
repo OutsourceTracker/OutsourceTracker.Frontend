be2fffd50a62d5ac66755af0c28546eec80ddb6d
Austin Vandersluis <austin@vandersluis.club>
2026-05-04 01:02:06 -0700

    Add OrganizationalUnitService for backend interactions

---
+++ src/Services/OrganizationalUnitService.cs
@@ -0,0 +1,14 @@
+﻿using OutsourceTracker.BusinessUnit.Divisions;
+
+namespace OutsourceTracker.Services;
+
+public class OrganizationalUnitService : BaseBackendService<Guid, OrganizationalUnit>
+{
+    protected override string EndPoint => "OU";
+
+    public OrganizationalUnitService(IServiceProvider services) : base(services)
+    {
+    }
+
+    
+}
