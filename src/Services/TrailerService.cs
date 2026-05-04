8779cf38b4d5dd3fe377a092965adbb812a3e1a9
Austin Vandersluis <austin@vandersluis.club>
2026-05-04 01:02:06 -0700

    Refactor: Adapt TrailerService to new ModelResult pattern

--- src/Services/TrailerService.cs
+++ src/Services/TrailerService.cs
@@ -1,77 +1,19 @@
 ﻿using OutsourceTracker.Geolocation;
 using OutsourceTracker.Models.Trailers;
-using OutsourceTracker.Services.ModelService;
+using OutsourceTracker.Services.DataModels;
 using System.Net.Http.Json;
-using System.Runtime.CompilerServices;
 
 namespace OutsourceTracker.Services;
 
-public class TrailerService : BaseBackendService, IModelCreateService<TrailerViewModel, HttpResponseMessage>, IModelLookupService<TrailerViewModel>, IModelDeleteService<TrailerViewModel, HttpResponseMessage>, IModelUpdateService<TrailerViewModel, HttpResponseMessage>, ITrackableLocationService<TrailerViewModel, HttpResponseMessage>
+public class TrailerService : BaseBackendService<Guid, TrailerViewModel>
 {
+    protected override string EndPoint => "trailers";
+
     public TrailerService(IServiceProvider sp) : base(sp)
     {
     }
 
-    public async Task<HttpResponseMessage> Create(TrailerViewModel? model = null, CancellationToken cancellationToken = default)
-    {
-        using HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Post, "trailers");
-        
-        if (model is not null)
-        {
-            message.Content = JsonContent.Create(model);
-        }
-
-        return await SendMessage(message, cancellationToken);
-    }
-
-    public async Task<TrailerViewModel?> Get(Guid id, CancellationToken cancellationToken = default)
-    {
-        if (id == Guid.Empty)
-        {
-            throw new ArgumentException("Value cannot be empty.", nameof(id));
-        }
-
-        var message = new HttpRequestMessage(HttpMethod.Get, $"trailers/{id}");
-        using var response = await SendMessage(message, cancellationToken);
-
-        response.EnsureSuccessStatusCode();
-        return await response.Content.ReadFromJsonAsync<TrailerViewModel>();
-    }
-
-    public async IAsyncEnumerable<TrailerViewModel> Search(object? searchOptions = null, [EnumeratorCancellation] CancellationToken cancellationToken = default)
-    {
-        using HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Get, "trailers");
-        using HttpResponseMessage response = await SendMessage(message, cancellationToken);
-        response.EnsureSuccessStatusCode();
-        var haystack = response.Content.ReadFromJsonAsAsyncEnumerable<TrailerViewModel>(cancellationToken);
-        await foreach (var item in haystack)
-        {
-            if (item != null)
-            {
-                yield return item;
-            }
-        }
-    }
-
-    public async Task<HttpResponseMessage> Delete(Guid id, CancellationToken cancellationToken = default)
-    {
-        HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Delete, $"trailers/{id}");
-        return await SendMessage(message, cancellationToken);
-    }
-
-    public async Task<HttpResponseMessage> Update(Guid id, object request, CancellationToken cancellationToken = default)
-    {
-        HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Put, $"trailers/{id}");
-
-        if (request != null)
-        {
-            message.Content = JsonContent.Create(request);
-        }
-
-        return await SendMessage(message, cancellationToken);
-    }
-
-    public async Task<HttpResponseMessage> UpdateLocation(Guid id, Vector2 mapCoordinates, double? accuracy = null, CancellationToken cancellationToken = default)
+    public async Task<ModelResult> UpdateLocation(Guid id, Vector2 mapCoordinates, double? accuracy = null, CancellationToken cancellationToken = default)
     {
         string messageUri = $"/trailers/{id}/spot";
 
@@ -80,8 +22,19 @@ public class TrailerService : BaseBackendService, IModelCreateService<TrailerVie
             messageUri += $"?acc={accuracy.Value}";
         }
 
-        HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Put, messageUri);
+        using HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Put, messageUri);
         message.Content = JsonContent.Create(mapCoordinates);
-        return await SendMessage(message, cancellationToken);
+        using HttpResponseMessage response = await SendMessage(message, cancellationToken);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var result = await ForceCacheUpdateAsync(id, cancellationToken);
+            return ModelResult.Builder().WithResult(result).WithSuccess().Build();
+        }
+        else
+        {
+            string errorContent = await response.Content.ReadAsStringAsync(cancellationToken);
+            return ModelResult.Builder().AddError("ServerError", errorContent).Build();
+        }
     }
 }
