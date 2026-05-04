b043d3b8c880133dd4b93065df1cc224e1d64dc8
Austin Vandersluis <austin@vandersluis.club>
2026-05-04 01:02:06 -0700

    Add AccountService for backend interactions

---
+++ src/Services/AccountService.cs
@@ -0,0 +1,12 @@
+﻿using OutsourceTracker.BusinessUnit.Accounts;
+
+namespace OutsourceTracker.Services;
+
+public class AccountService : BaseBackendService<Guid, OrganizationalAccount>
+{
+    protected override string EndPoint => "Account";
+
+    public AccountService(IServiceProvider services) : base(services)
+    {
+    }
+}
